0332b5098097c52291794e5e0dc90b04a30cc117
Khushal Khan <khushalkhan40@gmail.com>
2023-08-04 12:15:35 +0500

    added products to shop
    product context  created

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -25,7 +25,7 @@ const SignUpForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (password != confirmPassword) {
+    if (password !== confirmPassword) {
       alert("your password doesnt match");
       return;
     }
